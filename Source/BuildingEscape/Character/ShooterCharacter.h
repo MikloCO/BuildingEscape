ae8eb458776a3fdf22f38b7d192cf0dda13e1ca5
MikloCO <37591806+MikloCO@users.noreply.github.com>
2021-04-06 22:16:56 +0200

    Working on Level 2
    - Added new character
    - Created new C++ class
    - New Shooter BP_Class
    - Created new level
    - Added new BP_Gamemode
    - Started implemeneting character controller
    - Created input mappings / bindnings

---
+++ Source/BuildingEscape/Character/ShooterCharacter.h
@@ -0,0 +1,38 @@
+// Copyright Olivia Mikler 2020-2021
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+UCLASS()
+class BUILDINGESCAPE_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AShooterCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private: 
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+	void LookUpRate(float AxisValue);
+	void LookRightRate(float AxisValue);
+
+	UPROPERTY(EditAnywhere)
+		float RotationRate = 10;
+	
+};
