f8ddcacb2f5abf6c9fb159f31e85d74d21e07dd5
MikloCO <37591806+MikloCO@users.noreply.github.com>
2021-04-14 17:30:32 +0200

    Added gun c++
    - Created BP_Gun & setup the attachments (Root = root, sketalmesh = basemesh)

---
+++ Source/BuildingEscape/Character/Gun.cpp
@@ -0,0 +1,32 @@
+// Copyright Olivia Mikler 2020-2021
+#include "Gun.h"
+#include "Components/SkeletalMeshComponent.h"
+
+// Sets default values
+AGun::AGun()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root Component"));
+	RootComponent = Root;
+
+	Mesh = CreateDefaultSubobject< USkeletalMeshComponent>(TEXT("Base Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+
+}
+
+// Called when the game starts or when spawned
+void AGun::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGun::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
